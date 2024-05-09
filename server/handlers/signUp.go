63550d901a8bc1c61d86200445a3b67cad8698d7
pro0o <pp48041721@student.ku.edu.np>
2024-05-09 20:53:32 +0545

    feat: auth.

---
+++ server/handlers/signUp.go
@@ -0,0 +1,47 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+	"secondLife/types"
+	"secondLife/utils"
+
+	"github.com/google/uuid"
+)
+
+func (h *APIServer) signUp(w http.ResponseWriter, r *http.Request) error {
+	var newUser types.User
+	if err := json.NewDecoder(r.Body).Decode(&newUser); err != nil {
+		return err
+	}
+
+	if h.store.CheckEmailExists(newUser.Email) {
+		return utils.WriteJSON(w, http.StatusForbidden, "user already exists")
+	}
+
+	if err := h.store.CreateUser(&newUser); err != nil {
+		return err
+	}
+
+	code := utils.GenerateRandomCode()
+
+	if err := SendVerificationCode(newUser.Email, code); err != nil {
+		return err
+	}
+
+	responseData := struct {
+		UserID         uuid.UUID `json:"user_id"`
+		ProfilePicture int       `json:"profile_picture"`
+		UserName       string    `json:"user_name"`
+	}{
+		UserID:         newUser.UserID,
+		ProfilePicture: newUser.ProfilePicture,
+		UserName:       newUser.UserName,
+	}
+
+	if err := utils.WriteJSON(w, http.StatusOK, responseData); err != nil {
+		return err
+	}
+
+	return nil
+}
