91741fdc2aaa2fd8583943e1c920e8f391d112c8
pro0o <pp48041721@student.ku.edu.np>
2024-05-09 15:52:11 +0545

    chore: initialization.

---
+++ server/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"secondLife/handlers"
+	"secondLife/storage"
+)
+
+func main() {
+	store, err := storage.NewDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := store.Init(); err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Connected to database.")
+	server := handlers.NewAPIServer(":8081")
+	server.Run()
+}
