595e09411d4bb0c3cc58681ac15f9df551b80adc
pro0o <pp48041721@student.ku.edu.np>
2024-05-10 10:01:50 +0545

    perf: parse the response from llm.

--- ollamaApi/main.py
+++ ollamaApi/main.py
@@ -1,7 +1,12 @@
 from flask import Flask, request, jsonify
-import ollama
+import ollama, json
 
 app = Flask(__name__)
+def extract_json_string(response_data):
+    start_index = response_data.find("[{")
+    end_index = response_data.rfind("}]") + 2
+    json_string = response_data[start_index:end_index]
+    return json_string
 
 @app.route('/recyclingData/tips', methods=['POST'])
 def chat():
@@ -10,8 +15,12 @@ def chat():
         response = ollama.chat(model='llama3', messages=[{'role': 'user', 'content': content}])
 
         response_content = response['message']['content']
+        json_string = extract_json_string(response_content)
+        print(json_string)
+        json_data = json.loads(json_string)
+        print(json_data)
 
-        return jsonify({'response': response_content})
+        return jsonify({'response': json_data})
     else:
         return jsonify({'error': 'Request must be in JSON format'}), 400
 
