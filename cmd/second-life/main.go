77791e4e855401fc0997a07d4f563a6c17494f94
pro0o <pp48041721@student.ku.edu.np>
2025-03-30 21:31:31 +0545

    chore: connectrpc+jet.

---
+++ cmd/second-life/main.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"net/http"
+	"os"
+
+	storagev1connect "github.com/pro0o/second-life/gen/bowie/v1/storagev1connect"
+
+	"connectrpc.com/connect"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/rs/zerolog/log"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
+
+	"github.com/pro0o/second-life/internal/data"
+	"github.com/pro0o/second-life/internal/handler"
+)
+
+const (
+	defaultPort = "8080"
+)
+
+func main() {
+	os.Exit(run())
+}
+
+func run() int {
+	ctx := context.Background()
+
+	logger := log.With().Str("service", "github.com/pro0o/second-life").Logger()
+	err := godotenv.Load()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+	addr := "0.0.0.0:" + port
+
+	dsn := os.Getenv("DSN")
+	log.Printf("dsn is:", dsn)
+	if dsn == "" {
+		logger.Error().Msg("DSN environment variable is required")
+		return 1
+	}
+
+	// jwtSecret := os.Getenv("JWT_SECRET")
+	// if jwtSecret == "" {
+	// 	logger.Error().Msg("JWT_SECRET environment variable is required")
+	// 	return 1
+	// }
+
+	db, err := setupDatabase(dsn)
+	if err != nil {
+		logger.Error().Err(err).Msg("failed to setup database")
+		return 1
+	}
+	defer db.Close()
+
+	repo := data.NewRepository(ctx, db)
+	appHandler := handler.NewHandler(repo, logger)
+	router := setupRouter(appHandler)
+
+	logger.Info().Str("addr", addr).Msg("starting server")
+	err = http.ListenAndServe(
+		addr,
+		h2c.NewHandler(router, &http2.Server{}),
+	)
+
+	if err != nil {
+		logger.Error().Err(err).Msg("server error")
+		return 1
+	}
+
+	return 0
+}
+
+func setupDatabase(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, fmt.Errorf("opening database: %w", err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, fmt.Errorf("pinging database: %w", err)
+	}
+
+	return db, nil
+}
+
+func setupRouter(appHandler *handler.Handler) chi.Router {
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.RealIP)
+
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
+	r.Mount(storagev1connect.NewStorageServiceHandler(appHandler, connect.WithInterceptors(
+		handler.ServiceVersionInterceptor("StorageService", "v1"),
+		handler.LoggingInterceptor(&appHandler.Log),
+	)))
+	return r
+}
